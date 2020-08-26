6ba1d03c372c1442cda76acf51e50cfca89cecf6
dakatk <dakatk97@gmail.com>
2020-08-26 10:01:34 -0500

    Updated CLI arg parsing in 'main'

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,41 @@
 mod nn;
 mod parse_json;
 
+#[macro_use]
+extern crate clap;
+
 use nn::network::Network;
-
 use serde_json::Value;
-
-use std::env;
+use clap::App;
 use std::fs;
 
 #[doc(hidden)]
 fn main() -> Result<(), String> {
-    let args: Vec<String> = env::args().collect();
+    let yaml_cli = load_yaml!("cli.yml");
+    let args = App::from_yaml(yaml_cli).get_matches();
 
-    let filename = match args.len() {
-        2 => args[1].to_string(),
-        _ => return Err("Incorrect number of args".to_string())
+    let network_filename: &str = args.value_of("network").unwrap();
+    let data_filename: &str = args.value_of("data").unwrap();
+
+    let network_contents: String;
+    let data_contents: String;
+
+    match fs::read_to_string(&network_filename) {
+        Ok(result) => network_contents = result,
+        Err(_) => return Err(format!("'{}' missing or corrupted", network_filename).to_string())
     };
 
-    let file_contents: String;
-
-    match fs::read_to_string(&filename) {
-        Ok(result) => file_contents = result,
-        Err(_) => return Err(format!("'{}' missing or corrupted", filename).to_string())
+    match fs::read_to_string(&data_filename) {
+        Ok(result) => data_contents = result,
+        Err(_) => return Err(format!("'{}' missing or corrupted", data_filename).to_string())
     };
 
-    let network_json: Value = serde_json::from_str(&file_contents).unwrap();
+    let network_json: Value = serde_json::from_str(&network_contents).expect("Failed to parse JSON contents from network file");
+    let data_json: Value = serde_json::from_str(&data_contents).expect("Failed to parse JSON contents from data file");
 
     if !parse_json::has_keys(
         &network_json,
         vec![
-            "input",
-            "output",
             "cost",
             "hidden_layers",
             "optimizer",
@@ -40,11 +45,17 @@ fn main() -> Result<(), String> {
             "epochs",
         ]
     ) {
-        return Err("Invalid JSON file (missing required keys)".to_string());
+        return Err("Invalid network JSON file (missing required keys)".to_string());
     }
 
-    let input_json = network_json["input"].as_object().unwrap();
-    let output_json = network_json["output"].as_object().unwrap();
+    if !parse_json::has_keys(
+        &data_json, vec!["input", "output"]
+    ) {
+        return Err("Invalid data JSON file (missing required keys".to_string());
+    }
+
+    let input_json = data_json["input"].as_object().unwrap();
+    let output_json = data_json["output"].as_object().unwrap();
     let cost_json = network_json["cost"].as_str().unwrap();
     let layers_json = network_json["hidden_layers"].as_array().unwrap();
     let optimizer_json = network_json["optimizer"].as_object().unwrap();
