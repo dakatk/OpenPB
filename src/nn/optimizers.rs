db2dacb1f77529eeeff1aedf282a5bb4074df3fc
dakatk <dakatk97@gmail.com>
2020-12-27 18:39:02 -0600

    Added momentum constants as fields for Adam

--- src/nn/optimizers.rs
+++ src/nn/optimizers.rs
@@ -26,13 +26,13 @@ pub struct SGD {
     /// The step size when adjusting weights for each call of gradient descent
     learning_rate: f64,
 
-    /// Set of velocity values for use in classical momentum to ensure that
-    /// steps are slowed down as they approach a minimum over time
-    velocities: Vec<Array2<f64>>,
-
     /// Momentum constant, typically set to 0.9 (`DEFAULT_BETA_1`) except 
     /// in certain edge cases
-    beta: f64
+    beta: f64,
+
+    /// Set of velocity values for use in classical momentum to ensure that
+    /// steps are slowed down as they approach a minimum over time
+    velocities: Vec<Array2<f64>>
 }
 
 impl SGD {
@@ -43,8 +43,8 @@ impl SGD {
     pub fn new(learning_rate: f64, beta: f64) -> SGD {
         SGD {
             learning_rate,
-            velocities: vec![],
-            beta
+            beta,
+            velocities: vec![]
         }
     }
 }
@@ -78,11 +78,21 @@ pub struct Adam {
     /// The step size when adjusting weights during gradient descent
     learning_rate: f64,
 
+    /// Momentum constant, typically set to 0.9 (`DEFAULT_BETA_1`) except 
+    /// in certain edge cases
+    beta1: f64,
+
+    /// Secondary momentum constant, typically set to 0.999 (`DEFAULT_BETA_2`) except 
+    /// in certain edge cases
+    beta2: f64,
+
     /// Set of velocity values for use in RMS propogation
     velocities: Vec<Array2<f64>>,
 
     /// Set of moment values for use in classical momentum
-    moments: Vec<Array2<f64>>
+    moments: Vec<Array2<f64>>,
+
+
 }
 
 impl Adam {
@@ -90,10 +100,12 @@ impl Adam {
     ///
     /// * `learning_rate` - The step size when adjusting weights during gradient descent
     #[allow(dead_code)]
-    pub fn new(learning_rate: f64) -> Adam {
+    pub fn new(learning_rate: f64, beta1: f64, beta2: f64) -> Adam {
         Adam {
             time_step: 0,
             learning_rate,
+            beta1,
+            beta2,
             velocities: vec![],
             moments: vec![]
         }
@@ -116,29 +128,29 @@ impl Optimizer for Adam {
                 self.moments.push(Array2::zeros(delta_weights.dim()));
             }
 
-            let moment: Array2<f64> = (&self.moments[i] * DEFAULT_BETA_1) + (&delta_weights * (1. - DEFAULT_BETA_1));
+            let moment: Array2<f64> = (&self.moments[i] * self.beta1) + (&delta_weights * (1. - self.beta1));
 
             let velocity: Array2<f64> = {
                 let grad_squard = delta_weights.mapv(|el| el * el);
-                (&self.velocities[i] * DEFAULT_BETA_2) + (grad_squard * (1. - DEFAULT_BETA_2))
+                (&self.velocities[i] * self.beta2) + (grad_squard * (1. - self.beta2))
             };
 
             self.moments[i].assign(&moment);
             self.velocities[i].assign(&velocity);
 
             let moment_bar: Array2<f64> = {
-                let beta1_t = 1. - DEFAULT_BETA_1.powi(self.time_step as i32);
+                let beta1_t = 1. - self.beta1.powi(self.time_step as i32);
                 self.moments[i].mapv(|el| el / beta1_t)
             };
 
             let velocity_sqrt: Array2<f64> = {
-                let beta2_t = 1. - DEFAULT_BETA_2.powi(self.time_step as i32);
+                let beta2_t = 1. - self.beta2.powi(self.time_step as i32);
                 let velocity_bar: Array2<f64> = self.velocities[i].mapv(|el| el / beta2_t);
 
                 velocity_bar.mapv(|el| f64::sqrt(el) + 1e-7)
             };
 
-            let moment_adj = (moment_bar * self.learning_rate) / velocity_sqrt;
+            let moment_adj: Array2<f64> = (moment_bar * self.learning_rate) / velocity_sqrt;
 
             layers[i].update(&moment_adj, &delta_biases)
         }
